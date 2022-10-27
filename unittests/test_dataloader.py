b1c5dafec7c95cc3e1f7677795571a5e2e20cfa1
kaidong <dongforregister@gmail.com>
2022-10-26 21:35:19 -0400

    testing

---
+++ unittests/test_dataloader.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestDataLoader(unittest.TestCase):
+    maxDiff = None
+
+    def test_load_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
